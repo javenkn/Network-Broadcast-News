ac0dcdf6feb393e54f1290c3774584ca6633d1b5
javenkn <javenkn7@yahoo.com>
2016-06-29 18:21:08 -1000

    Made it able to see the whole chat on each client

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ var net = require('net');
 
 var CONFIG = require('./config');
 
+var connectedSockets = [];
+
 var server = net.createServer(function (socket) { //readable socket
   //connection listener
   console.log('Somebody connected!');
-
+  connectedSockets.push(socket);
   socket.setEncoding('utf8');
 
   socket.on('data', (data) => {
     console.log(data);
-    socket.write(data);
+    connectedSockets.forEach(function (element) {
+      element.write(data);
+    });
   });
 
   socket.on('end', () => {
